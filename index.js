24c27e30c93ce9ca8c3ecf0f849af3b2853b7d75
mcm1957 <mcm57@gmx.at>
2023-06-06 21:36:55 +0200

    intermediate upload

--- index.js
+++ index.js
@@ -3,13 +3,39 @@
 // const mdTable = require('markdown-table');
 // const utils = require('./lib/utils');
 // const fs = require('fs');
-const repoTools = require('./lib/repoTools.js');
+const labelTools = require('./lib/labelTools.js');
+const commandLineArgs = require('command-line-args');
 
 //let maintainers = JSON.parse(fs.readFileSync('maintainers.json'));
 
-if (process.argv[2].toLowerCase() === 'listlabels') {
-  const repo = process.argv[3];
-  repoTools.listLabels( repo );
+const optDef = [
+    { name: 'para', type: String, multiple: true, defaultOption: true },
+    { name: 'debug', alias: 'd', type: Boolean },
+    { name: 'verbose', alias: 'v', type: Boolean },
+];
+
+let args;
+try {
+    args = commandLineArgs(optDef);
+} catch (e) {
+    if (e.name === 'UNKNOWN_OPTION') {
+        console.log(`error: unknown option '${e.optionName}'`);
+        process.exit(1);
+    } else if (e.name === 'UNKNOWN_VALUE') {
+        console.log(`error: unsupported option value '${e.optionValue}'`);
+        process.exit(1);
+    } else if (e.name === 'ALREADY_SET') {
+        console.log(`error: option '${e.optionName}' specified multiple times`);
+        process.exit(1);
+    } else {
+        throw e;
+    }
+}
+
+if (args.para[0] && args.para[0] === 'list-labels') {
+    labelTools.listLabels(args);
+} else if (args.para[0] && args.para[0] === 'set-labels') {
+    labelTools.setLabels(args);
 } else {
-  console.log(`unknown command ${process.argv[2]}`);
+    console.log(`unknown command ${args.para[0]}`);
 }
